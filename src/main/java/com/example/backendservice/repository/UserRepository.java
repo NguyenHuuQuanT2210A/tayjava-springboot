21d460698ff5bc8596d3e413b211a690d8e1642d
NguyenHuuQuanT2210A <118426160+NguyenHuuQuanT2210A@users.noreply.github.com>
2025-04-26 15:37:46 +0700

    new branch

--- src/main/java/com/example/backendservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backendservice.repository;
-
-import com.example.backendservice.model.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
